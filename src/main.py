541a6f86cd9453717dd69a2006dd6e30a8b87111
Moroni Motta <53498888+moronimotta@users.noreply.github.com>
2023-05-31 22:45:55 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -13,7 +13,7 @@ login_info = []
 
 # connect to database
 connection = mysql.connector.connect(
-    host="db_local_byui",
+    host="localhost",
     user="admin",
     password="root",
     database="grocerystore"
